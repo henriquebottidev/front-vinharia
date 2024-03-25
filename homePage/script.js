7fcbe9fbc2b26aa04aafa753df2eb678dbddf0e9
henriquebottidev <henriquembotti@gmail.com>
2024-03-25 09:49:10 -0300

    header da loja adicionado

--- homePage/script.js
+++ homePage/script.js
@@ -7,3 +7,12 @@ function verifyAge(answer) {
 
     document.getElementById('popup').style.display = "none"
 }
+
+function goStore(answer) {
+    if(answer == 'loja') {
+        window.location.href = "storePage.html"; 
+    } else if (answer == 'nao') {
+        window.location.href = "https://www.google.com";
+    }
+    document.getElementById('loja').style.display = "none"
+}
